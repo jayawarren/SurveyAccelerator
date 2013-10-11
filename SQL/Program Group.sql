6ba1eb90eebdd5ea5acf3c5323a5d89cb98d194c
jayawarren <jay.warren@data-alt.com>
2013-10-11 14:59:43 -0400

    Commit

--- SQL/Program Group.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[Program Group] (
-[ORGANIZATION_NAME] varchar(75),
-[OFF_BR_NUM] varchar(10),
-[Program_Type] varchar(75),
-[Program_Site_Location] varchar(100),
-[Group_Name] varchar(75),
-[CreateDateTime] [datetime] DEFAULT GETDATE()
-) ON DIMGROUP
